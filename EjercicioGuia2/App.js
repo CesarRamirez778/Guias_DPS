a11b5dce396054c6905b4d02fef9c4a0f3e9f045
Cesar Ramírez <61605947+CesarRamirez778@users.noreply.github.com>
2022-11-11 18:55:18 -0600

    Update App.js

--- EjercicioGuia2/App.js
+++ EjercicioGuia2/App.js
@@ -1,12 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+import useState from "react"
+// You can import from local files
+import AssetExample from './components/AssetExample';
+
+// or any pure javascript modules available in npm
+import { Card } from 'react-native-paper';
+
+Const form =>(){
+  const[todos,setTodos]=useState([
+    {todo: 'todo1'},
+    {todo: 'todo2'},
+    {todo: 'todo3'}
+  ])
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.paragraph}>
+        TO-DO LIST
+      </Text>
+      <Card>
+       
+      </Card>
+     
     </View>
   );
 }
@@ -14,8 +33,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+    padding: 8,
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
 });
