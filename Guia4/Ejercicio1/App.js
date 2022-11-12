18129f311a68ff82ef7b1df5bc50114942f39a7e
Cesar Ramírez <61605947+CesarRamirez778@users.noreply.github.com>
2022-11-11 19:23:27 -0600

    Update App.js

--- Guia4/Ejercicio1/App.js
+++ Guia4/Ejercicio1/App.js
@@ -1,52 +1,29 @@
-import {AppRegistry} from 'react-native';
-import React from "react";
-import { SafeAreaView, View, FlatList, StyleSheet, Text, StatusBar } from 'react-native';
-const DATA = [
-{
-id: '1',
-title: 'Toyota',
-},
-{
-id: '2',
-title: 'Mazda',
-},
-{
-id: '3',
-title: 'Mitsubishi',
-},
-];
-const Item = ({ title }) => (
-<View style={styles.item}>
-<Text style={styles.title}>{title}</Text>
+
+import React from 'react';
+import {
+SafeAreaView,
+StyleSheet,
+View,
+Text,
+} from 'react-native';
+export default function App(){
+return(
+<>
+<SafeAreaView style={styles.Header}>
+<Text>Header</Text>
+</SafeAreaView>
+<View>
+<Text>Result</Text>
 </View>
-);
-const App = () => {
-const renderItem = ({ item }) => (
-<Item title={item.title} />
-);
-return (
-<SafeAreaView style={styles.container}>
-<FlatList
-data={DATA}
-renderItem={renderItem}
-keyExtractor={item => item.id}
-/>
-</SafeAreaView> 
+<View>
+<Text>Footer</Text>
+</View>
+</>
 );
 }
 const styles = StyleSheet.create({
-container: {
-flex: 1,
-marginTop: StatusBar.currentHeight || 0,
-},
-item: {
-backgroundColor: '#f9c2ff',
-padding: 20,
-marginVertical: 8,
-marginHorizontal: 16,
-},
-title: {
-fontSize: 32,
-},
-});
-AppRegistry.registerComponent("cars", () => App);
+Header:{
+backgroundColor:'blue',
+height:200,
+borderBottomLeftRadius:30,
+borderBottomRightRadius:30,
